2c526cdddf57861e42abf5a608bd483536c97484
Muhammad Ubaid Raza <mubaidr@gmail.com>
2017-06-15 22:19:05 +0500

    Create svg-to-image.js

--- svg-to-image.js
+++ svg-to-image.js
@@ -1,18 +1,18 @@
-function svgToPNG(svg, width, height, callback) {
+function svgToImage(svg, width, height, type, callback) {
 	var svgString = new XMLSerializer().serializeToString(svg);
 	var canvas = document.createElement('canvas');
 	canvas.width = width;
 	canvas.height = height;
-	var ctx = canvas.getContext("2d");
+	var ctx = canvas.getContext('2d');
 	var DOMURL = self.URL || self.webkitURL || self;
 	var img = new Image();
 	var svg = new Blob([svgString], {
-		type: "image/svg+xml;charset=utf-8"
+		type: 'image/svg+xml;charset=utf-8'
 	});
 	var url = DOMURL.createObjectURL(svg);
 	img.onload = function () {
 		ctx.drawImage(img, 0, 0, width, height);
-		var png = canvas.toDataURL("image/png");
+		var png = canvas.toDataURL('image/' + type);
 		if (callback) {
 			callback(png);
 		}
