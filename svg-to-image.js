9caf38ff31b6d2b663c92b98a1f177ea6d86784e
Muhammad Ubaid Raza <mubaidr@gmail.com>
2017-06-15 23:51:59 +0500

    Fix firefox render issue

--- svg-to-image.js
+++ svg-to-image.js
@@ -1,21 +1,26 @@
-function svgToImage(svg, width, height, type, callback) {
-	var svgString = new XMLSerializer().serializeToString(svg);
+function svgToImage(svgNodeOriginal, width, height, type, callback) {
+	//ADD width, height attribtutes, otherwise firefox won't render svg into Canvas
+	var svgNode = svgNodeOriginal.cloneNode(true);
+	svgNode.setAttribute('width', width + 'px')
+	svgNode.setAttribute('height', height + 'px')
+
+	var svgString = new XMLSerializer().serializeToString(svgNode);
 	var canvas = document.createElement('canvas');
 	canvas.width = width;
 	canvas.height = height;
 	var ctx = canvas.getContext('2d');
-	var DOMURL = self.URL || self.webkitURL || self;
+	//var DOMURL = self.URL || self.webkitURL || self;
 	var img = new Image();
-	var svg = new Blob([svgString], {
-		type: 'image/svg+xml;charset=utf-8'
-	});
-	var url = DOMURL.createObjectURL(svg);
+	// var svg = new Blob([svgString], {
+	// 	type: 'image/svg+xml;charset=utf-8'
+	// });
+	//var url = DOMURL.createObjectURL(svg);
 	img.onload = function () {
 		ctx.drawImage(img, 0, 0, width, height);
-		var png = canvas.toDataURL('image/' + type);
-		if (callback) {
-			callback(png);
-		}
+		var output = canvas.toDataURL('image/' + type);
+		callback(output);
 	};
-	img.src = url;
+	//img.src = url;
+	img.src = "data:image/svg+xml;base64," + window.btoa(unescape(encodeURIComponent(svgString)));
+	//img.src = "data:image/svg+xml;charset=utf-8," + window.btoa(unescape(encodeURIComponent(svgString)));
 }
