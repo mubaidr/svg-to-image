0d84ad53f8e2291471c7d68f28825d9eaff6a901
Muhammad Ubaid Raza <mubaidr@gmail.com>
2017-06-16 08:50:36 +0500

    Update svg-to-image.js

--- svg-to-image.js
+++ svg-to-image.js
@@ -1,3 +1,12 @@
+/**
+ * SVG TO Image
+ * 
+ * @param {any} svgNodeOriginal SVG Element
+ * @param {any} width Width of final Image
+ * @param {any} height Height of final Image
+ * @param {any} type Types of Image ('jpeg', 'png')
+ * @param {any} callback Callback function with Image DATAURL as only argument
+ */
 function svgToImage(svgNodeOriginal, width, height, type, callback) {
 	//ADD width, height attribtutes, otherwise firefox won't render svg into Canvas
 	var svgNode = svgNodeOriginal.cloneNode(true);
@@ -9,18 +18,12 @@ function svgToImage(svgNodeOriginal, width, height, type, callback) {
 	canvas.width = width;
 	canvas.height = height;
 	var ctx = canvas.getContext('2d');
-	//var DOMURL = self.URL || self.webkitURL || self;
+
 	var img = new Image();
-	// var svg = new Blob([svgString], {
-	// 	type: 'image/svg+xml;charset=utf-8'
-	// });
-	//var url = DOMURL.createObjectURL(svg);
 	img.onload = function () {
 		ctx.drawImage(img, 0, 0, width, height);
 		var output = canvas.toDataURL('image/' + type);
 		callback(output);
 	};
-	//img.src = url;
 	img.src = "data:image/svg+xml;base64," + window.btoa(unescape(encodeURIComponent(svgString)));
-	//img.src = "data:image/svg+xml;charset=utf-8," + window.btoa(unescape(encodeURIComponent(svgString)));
 }
